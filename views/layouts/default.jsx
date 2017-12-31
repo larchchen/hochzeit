ca71e7e7fb299db3e7aeab2675c01a9a43448ece
Lin Chen <larch.lin.chen@gmail.com>
2017-12-31 18:40:31 +0000

    Add skeleton UI

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -7,6 +7,8 @@ class DefaultLayout extends React.Component {
         <head>
           <title>{this.props.title}</title>
           <link rel='stylesheet' href='/static/stylesheets/style.css' />
+          <link rel='stylesheet' href='/static/stylesheets/normalize.css' />
+          <link rel='stylesheet' href='/static/stylesheets/skeleton.css' />
           <script type="text/javascript" src="/static/javascripts/bundle.js" />
         </head>
         <body>{this.props.children}</body>
