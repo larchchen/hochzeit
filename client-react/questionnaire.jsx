16f835d5554281cdfb17a1117e002421eedb8be7
Lin Chen <larchchen@fb.com>
2018-01-03 11:35:30 +0000

    Add arrival time and casual dinner row in questionnaire

--- client-react/questionnaire.jsx
+++ client-react/questionnaire.jsx
@@ -18,7 +18,8 @@ class Questionaire extends React.Component {
     this.state = {
       stage: 0,
       mainGuestName: '',
-      otherGuests: []
+      otherGuests: [],
+      arrivalTime: 0
     };
     this.handleMainGuestNameChange = this.handleMainGuestNameChange.bind(this);
     this.handleOtherGuestNameChange = this.handleOtherGuestNameChange.bind(this);
@@ -47,6 +48,14 @@ class Questionaire extends React.Component {
     let otherGuests = [...this.state.otherGuests];
     otherGuests.splice(index, 1);
     this.setState({otherGuests: otherGuests});
+  }
+
+  setArrivalTime(option, event) {
+    this.setState({arrivalTime: option});
+  }
+
+  setCasualDinner(option, event) {
+    this.setState({casualDinner: option});
   }
 
   getGuestNameRows() {
@@ -134,12 +143,82 @@ class Questionaire extends React.Component {
     );
   }
 
+  getArrivalTimeRow() {
+    return (
+      <div>
+        <div className="row">
+          <h5>When will you arrive at our wedding castle?</h5>
+        </div>
+        <div className="row">&nbsp;</div>
+        <div className="row">
+          <div className="four columns">
+            <input
+              type="button"
+              value="Morning, 5th Feb"
+              className={this.state.arrivalTime === 1 ? "button-clicked" : undefined}
+              onClick={(e) => this.setArrivalTime(1, e)}
+            />
+          </div>
+          <div className="four columns">
+            <input
+              type="button"
+              value="Afternoon, 5th Feb"
+              className={this.state.arrivalTime === 2 ? "button-clicked" : undefined}
+              onClick={(e) => this.setArrivalTime(2, e)}
+            />
+          </div>
+          <div className="four columns">
+            <input
+              type="button"
+              value="Morning, 6th Feb"
+              className={this.state.arrivalTime === 3 ? "button-clicked" : undefined}
+              onClick={(e) => this.setArrivalTime(3, e)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  getCasualDinnerRow() {
+    return (
+      <div>
+        <div className="row">
+          <h5>Would you like to join our family dinner the evening before?</h5>
+        </div>
+        <div className="row">&nbsp;</div>
+        <div className="row">
+          <div className="four columns">&nbsp;</div>
+          <div className="two columns">
+            <input
+              type="button"
+              value="Yes"
+              className={this.state.casualDinner ? "button-clicked" : undefined}
+              onClick={(e) => this.setCasualDinner(true, e)}
+            />
+          </div>
+          <div className="two columns">
+            <input
+              type="button"
+              value="No"
+              className={this.state.casualDinner === false ? "button-clicked" : undefined}
+              onClick={(e) => this.setCasualDinner(false, e)}
+            />
+          </div>
+          <div className="four columns">&nbsp;</div>
+        </div>
+      </div>
+    );
+  }
+
   getHiddenForm() {
     let guestNames = this.state.otherGuests.filter((g) => g.length > 0).join();
     return (
       <form action="" method="post" style={{display: "none"}}>
         <input type="hidden" name="mainGuestName" value={this.state.mainGuestName} />
         <input type="hidden" name="otherGuestNames" value={guestNames} />
+        <input type="hidden" name="arrivalTime" value={this.state.arrivalTime} />
+        <input type="hidden" name="casualDinner" value={!!this.state.casualDinner} />
       </form>
     );
   }
@@ -147,6 +226,11 @@ class Questionaire extends React.Component {
   render() {
     let hiddenForm = this.getHiddenForm();
     let guestNameRows = this.getGuestNameRows();
+    let arrivalTimeRow = this.getArrivalTimeRow();
+    let casualDinnerRow = null;
+    if (this.state.arrivalTime < 3) {
+      casualDinnerRow = <div>{this.getCasualDinnerRow()}<SectionSeperator /></div>;
+    }
     return (
       <div className="container">
         <div className="row">
@@ -157,6 +241,9 @@ class Questionaire extends React.Component {
         </div>
         {guestNameRows}
         <SectionSeperator />
+        {arrivalTimeRow}
+        <SectionSeperator />
+        {casualDinnerRow}
         {hiddenForm}
       </div>
     );
